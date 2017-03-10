a401c793aedf7c93a2554f4c02a21202f8812721
Daniel Hist Selli <danielhilst@gmail.com>
2017-03-10 03:38:01 -0300

    Add a little bit of JSON possibility.

---
+++ assets/js/post.js
@@ -0,0 +1,8 @@
+function Post(arg) {
+    this.title =  arg.title, 
+    this.date =   arg.date,
+    this.url =    arg.url,
+    this.id =     arg.id,
+    this.tags =   arg.tags;
+    this.path =   arg.path;
+}
