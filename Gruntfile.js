1c5e9c2290dca38e6f3e3e7c5261bb5057bee2a8
Daniel Hist Selli <danielhilst@gmail.com>
2017-03-27 00:58:32 -0300

    Setup grunt-lunr-index-generator.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,16 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        tagindex: {target: {}},
+        //tagindex: {target: {}},
+        lunr_index_generator: {
+            index: {
+                src: ['_site/**/*.html'],
+                dest: 'assets/js/posts-index.json'
+            }
+        }
     });
 
-    grunt.loadTasks('_site');
-    grunt.registerTask('default', ['tagindex']);
+    //grunt.loadTasks('_site');
+    //grunt.registerTask('default', ['tagindex']);
+    grunt.loadNpmTasks('grunt-lunr-index-generator');
+    grunt.registerTask('default', ['lunr_index_generator']);
 }
