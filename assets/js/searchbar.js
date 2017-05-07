bc88c6978943b98d37d6c0cfde458574ae7ac298
Daniel Hist Selli <danielhilst@gmail.com>
2017-05-07 03:36:09 -0300

    Improve search logic.

--- assets/js/searchbar.js
+++ assets/js/searchbar.js
@@ -14,19 +14,43 @@ $(() => {
 		const index = lunr(function() { 
 			this.ref('url');
 			this.field('title');
-			this.field('content');
 			workingPosts.forEach((post) => this.add(post));
 		});
 		$searchBar.keyup(() => {
 			const searchText = $searchBar.val();
 			if (searchText.length > 1) {
-				const results = index.search($searchBar.val());
+				var results = index.search(searchText);
 				if (results.length > 0) {
-					const list = results.map((r) => '<li><span class="post-tags-mark">&raquo;</span><a class="search-result" href="'+r.ref+'">' + store[r.ref].title + '</a></li>').join('');
-					$searchResults.empty().append('<ul class="search-result-list">' + list + '</ul>');
+                    console.log(results);
+                    // Remove unmatched
+                    $searchResults.find('li').filter(function(){
+                        return results.map(r => r.ref)
+                            .indexOf($(this).find('a').attr('href')) === -1;
+                    }).remove();
+                    // Remove dupplicates
+                    const $anchors = $searchResults.find('li a');
+                    if ($anchors.length > 0) {
+                        results = results.filter(function(r) {
+                            return $anchors.map(function(){
+                                return $(this).attr('href');
+                            }).toArray().indexOf(r.ref) === -1;
+                        });
+                        console.log('Filtred results: ' + results);
+                    }
+                    // Append result
+                    results.forEach((r) => {
+                        $searchResults.find('ul').append(
+                            $(["<li>",
+                               "  <span class='post-tags-mark'>&raquo;</span>",
+                               "  <a href='"+r.ref+"' class='search-result'>",
+                                    store[r.ref].title,
+                               "  </a>",
+                               "</li>"].join(''))
+                        );
+                    });
 				}
 			} else {
-				$searchResults.empty();
+				$searchResults.find('ul').empty();
 			}
 		});
 	});
