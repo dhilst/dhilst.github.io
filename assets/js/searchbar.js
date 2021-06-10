cbd372c53ef7a9d6350018e731500c5ee13c1424
Daniel Hilst Selli <daniel@versatushpc.com.br>
2021-06-10 00:45:51 -0300

    Fix site url and js indentation

--- assets/js/searchbar.js
+++ assets/js/searchbar.js
@@ -7,56 +7,56 @@ Object.defineProperty(Object.prototype, 'inspect', {
 $(() => {
 'use strict';
 
-	const $searchBar = $("#search-bar [type='text']"); 
-	const $searchResults = $('#search-results');
+  const $searchBar = $("#search-bar [type='text']");
+  const $searchResults = $('#search-results');
 
-	$searchBar.focus();
+  $searchBar.focus();
 
-    $searchResults.notIn = function(results){
-        return this.find('li').filter((_, li) =>
-            results.indexOf($(li).find('a').attr('href') === -1));
-    };
+  $searchResults.notIn = function(results){
+    return this.find('li').filter((_, li) =>
+      results.indexOf($(li).find('a').attr('href') === -1));
+  };
 
-    $searchResults.hrefs = function(){
-        return this.find('li a').map(a => $(a).attr('href')).toArray();
-    };
+  $searchResults.hrefs = function(){
+    return this.find('li a').map(a => $(a).attr('href')).toArray();
+  };
 
-    $searchResults.appendNew = function(results, store){
-       return results.filter(x => this.hrefs().indexOf(x) === -1).forEach(url =>
-           this.find('ul').append(
-               $(["<li>",
-                   "  <span class='post-tags-mark'>&raquo;</span>",
-                   "  <a href='"+url+"' class='search-result'>",
-                        store(url).title,
-                   "  </a>",
-                   "</li>"].join(''))));
-    };
+  $searchResults.appendNew = function(results, store){
+    return results.filter(x => this.hrefs().indexOf(x) === -1).forEach(url =>
+      this.find('ul').append(
+        $(["<li>",
+          "  <span class='post-tags-mark'>&raquo;</span>",
+          "  <a href='"+url+"' class='search-result'>",
+          store(url).title,
+          "  </a>",
+          "</li>"].join(''))));
+  };
 
 
-	$.getJSON('/posts.json', (posts) => {
-        posts.pop(); // Remove __SENTINEL__
+  $.getJSON('/posts.json', (posts) => {
+    posts.pop(); // Remove __SENTINEL__
 
-        const store = (function(posts){ 
-            var s = {};
-            posts.forEach(p => s[p.url] = p);
-            return function(url) { return s[url]; };
-        })(posts)
+    const store = (function(posts){
+      var s = {};
+      posts.forEach(p => s[p.url] = p);
+      return function(url) { return s[url]; };
+    })(posts)
 
-		const index = lunr(function() { 
-			this.ref('url');
-			this.field('title');
-            posts.forEach(post => this.add(post));
-		});
+    const index = lunr(function() {
+      this.ref('url');
+      this.field('title');
+      posts.forEach(post => this.add(post));
+    });
 
-		$searchBar.keyup(() => {
-            if ($searchBar.val().length > 0) {
-                const results = index.search($searchBar.val()).map(r => r.ref);
-                $searchResults.notIn(results).remove();
-                $searchResults.appendNew(results, store);
-            } else {
-                $searchResults.find('ul li').remove();
-            }
-		});
-	});
+    $searchBar.keyup(() => {
+      if ($searchBar.val().length > 0) {
+        const results = index.search($searchBar.val()).map(r => r.ref);
+        $searchResults.notIn(results).remove();
+        $searchResults.appendNew(results, store);
+      } else {
+        $searchResults.find('ul li').remove();
+      }
+    });
+  });
 });
 
