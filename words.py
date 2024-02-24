6f29af45f73ba1feb066c32c0557d6c777b0c0fd
Daniel Hilst Selli <danielhilst@gmail.com>
2024-02-23 21:45:15 -0300

    update words.py with tags

--- words.py
+++ words.py
@@ -1,3 +1,4 @@
+import yaml
 import random
 import re
 import operator as op
@@ -5,9 +6,19 @@ from typing import Any, Iterator
 from math import sqrt
 from collections import Counter
 from pathlib import Path
+from dataclasses import dataclass
 
 
 Vec = tuple[Counter, set[str], float]
+
+@dataclass(frozen=True)
+class Post:
+    path: str
+    title: str
+    tags: set[str]
+    vec: Vec
+    body: str
+    words: list[str]
 
 
 # https://stackoverflow.com/questions/29484529/cosine-similarity-between-two-words-in-a-list
@@ -30,19 +41,32 @@ def cosdis(v1: Vec, v2: Vec) -> float:
     return sum(v1[0][ch] * v2[0][ch] for ch in common) / v1[2] / v2[2]
 
 
-def read_posts() -> Iterator[tuple[str, list[str]]]:
+def read_posts() -> Iterator[Post]:
     for post in Path("./_posts/").glob("*.md"):
         with post.open() as post:
             content = post.read()
 
-        words = re.findall(r"\w+", content.split("---", 3)[2])
-        yield post.name, words
+        try:
+            head, body = content.split("---", 2)[1:]
+        except:
+            print(len(x), post)
+            raise RuntimeError
+        words = re.findall(r"\w+", body)
+        head = yaml.safe_load(head)
+        assert type(head) is dict
+        title = head.get("title", "No title")
+        tags = set(head.get("tags", []))
+        path = post.name
+        vec = words2vec(words)
+        post = Post(path, title, tags, vec, body, words)
+        yield post
 
 
 posts = list(read_posts())
-d: dict[str, Vec] = {pname: words2vec(words) for pname, words in posts}
+d: dict[str, Vec] = {post.path: post.vec for post in posts}
 
-pname, pwords = random.choice(posts)
+post = random.choice(posts)
+pname, pwords = post.path, post.words
 pvec = d[pname]
 
 dscored = sorted(
@@ -51,7 +75,38 @@ dscored = sorted(
     reverse=True,
 )
 
+print("All tags")
+tags = Counter(tag for p in posts for tag in p.tags)
+print(tags)
+
+print("Tags used once")
+tonce = [t for t, count in tags.items() if count == 1]
+print(f"{len(tonce) * 100/ len(tags):.02f}%")
+print(tonce)
+
+
+print("Posts wihtout tags")
+print(list(p.path for p in posts if not p.tags))
+
+
 print(f"Post: {pname}")
 print("Top 3 similar posts")
 for pname in dscored[0:3]:
     print(pname)
+
+def jaccard_distance(a: set[Any], b: set[Any]) -> float:
+    if not a or not b:
+        return 0
+
+    return len(a & b) / len(a | b)
+
+
+dscored_bytag = sorted(
+    ((p.path, jaccard_distance(p.tags, post.tags)) for p in posts if p.path != post.path),
+    key=op.itemgetter(1),
+    reverse=True
+)
+
+print("Top 3 similar posts by tag")
+for pname, d in dscored_bytag[0:3]:
+    print(pname, d)
